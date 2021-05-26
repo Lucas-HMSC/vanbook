2da9f3cc24fdf0989b426f3f4ab737779af39858
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-05-26 12:57:00 -0300

    Modal 6

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$('a.btn').on('click', function() {
+$('a.btn').on('click', function(e) {
+    e.preventDefault();
+
     $('.modal').addClass('modal-ativo');
-})
+});
+
+$('.modal').on('click', function(modal) {
+    if (modal.target == this) {
+        $(this).removeClass('modal-ativo');
+    }
+});

1fec7454c726f47c23bcd6f83daea8ab6fd6d445
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-05-27 13:10:41 -0300

    Icon Animation

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $('.modal').on('click', function(modal) {
 // Função para determinar o comprimento de cada path
 $('svg path').each(function() {
     var comprimento = $(this).get(0).getTotalLength();
-    var comprimentoArredondado = Math.riund(comprimento);
+    var comprimentoArredondado = Math.round(comprimento);
 
     $(this).attr('stroke-dasharray', comprimentoArredondado);
     $(this).attr('stroke-dashoffset', comprimentoArredondado);
